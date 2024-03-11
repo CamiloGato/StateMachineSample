102ad9be421ba90c8b08d1e3d8b4c0a1113dd2c1
Camilo Andres Rojas Hernandez <cm.andres022@hotmail.com>
2024-03-11 09:00:20 -0500

    Finished State Machine

--- Assets/Scripts/StateMachine/BaseStateMachine.cs
+++ Assets/Scripts/StateMachine/BaseStateMachine.cs
@@ -1,12 +1,21 @@
-﻿namespace StateMachine
+﻿using System;
+
+namespace StateMachine
 {
-    public abstract class BaseStateMachine
+    public abstract class BaseStateMachine<TEnum, TState> where TEnum : Enum where TState : BaseState
     {
-        public BaseState CurrentState { get; set; }
+        protected BaseState CurrentState { get; set; }
+        public abstract TransitionStateBuilder<TEnum, TState> Transition { get; set; }
         
-        public abstract void Enter();
+        public abstract void Initialize();
         public abstract void Close();
+
+        public virtual void TransitionTo(TEnum stateEnum)
+        {
+            CurrentState.ExitState();
+            CurrentState = Transition.GetState(stateEnum);
+            CurrentState.EnterState();
+        }
         
-        public abstract void ChangeState(BaseState baseState);
     }
 }
---
+++ Assets/Scripts/StateMachine/TransitionStateBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StateMachine
+{
+    public class TransitionStateBuilder<TEnum, TState>
+        where TEnum : Enum where TState : BaseState
+    {
+        private readonly Dictionary<TEnum, TState> _transitionsStates = new Dictionary<TEnum, TState>();
+
+        public TransitionStateBuilder<TEnum, TState> AddTransitionState( TEnum @enum, TState state )
+        {
+            _transitionsStates.Add(@enum, state);
+            return this;
+        }
+
+        public TransitionStateBuilder<TEnum, TState> Build()
+        {
+            return this;
+        }
+
+        public TState GetState(TEnum @enum)
+        {
+            if (_transitionsStates.TryGetValue(@enum, out TState state))
+            {
+                return state;
+            }
+
+            throw new Exception($"No States in {@enum}");
+        }
+    }
+}
