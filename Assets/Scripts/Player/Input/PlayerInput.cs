6fd76fcf569c6e034597cb383419a867eba8d7e3
Camilo Andres Rojas Hernandez <cm.andres022@hotmail.com>
2024-03-12 15:47:45 -0500

    Update

---
+++ Assets/Scripts/Player/Input/PlayerInput.cs
@@ -0,0 +1,10 @@
+﻿namespace Player.Input
+{
+    public class PlayerInput
+    {
+        public float Horizontal()
+        {
+            return 0;
+        }
+    }
+}
--- Assets/Scripts/Player/StateMachine/States/Jump/JumpSimplePlayerState.cs
+++ Assets/Scripts/Player/StateMachine/States/Jump/JumpSimplePlayerState.cs
@@ -1,22 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player.StateMachine.States.Jump
 {
     [CreateAssetMenu(fileName = "Simple", menuName = "Player/State Machine/Jump", order = 0)]
     public class JumpSimplePlayerState : JumpBasePlayerState
     {
+        public Rigidbody2D Rb { get; set; }
+        
+        
         public override void SetUp()
         {
-            Debug.Log("El pepe SetUp");
+            if (!Rb) throw new Exception("MISSING COMPONENT");
         }
 
-        public override void Dispose()
-        {
-            Debug.Log("El pepe Dispose");
-        }
+        public override void Dispose() {}
 
         public override void OnUpdate()
         {
+            
         }
 
         public override void OnFixedUpdate()
